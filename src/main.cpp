2f6bd5e767ac3f12b049586e808ad03f6c749caf
G Soros <gsoros@users.noreply.github.com>
2022-10-27 14:13:18 +0200

    .

--- src/main.cpp
+++ src/main.cpp
@@ -5,11 +5,6 @@
 Board board;
 
 void setup() {
-#ifdef CORE_DEBUG_LEVEL
-    esp_log_level_set("*", (esp_log_level_t)CORE_DEBUG_LEVEL);
-#else
-    esp_log_level_set("*", ESP_LOG_ERROR);
-#endif
     board.setup();
     board.startTasks();
 }
